9dd88d7b0af87479120caaf12bcaea494cb0c936
Hiep Dinh <hoanghiepitvnn@gmail.com>
2022-03-05 13:12:50 +0700

    Only inject 3rd libraries for specify domain

--- browser_extenstions/chrome/scripts/services/library_injector.js
+++ browser_extenstions/chrome/scripts/services/library_injector.js
@@ -2,14 +2,17 @@ const libraryInjector = () => {
   const libs = [
     {
       path: 'scripts/vendor/jquery.min.js',
+      matches: [],
       onload: () => {},
     },
     {
       path: 'scripts/vendor/bootstrap.bundle.min.js',
+      matches: [/https:\/\/.*fshare.vn\/folder\//],
       onload: () => {},
     },
     {
       path: 'scripts/vendor/toastr.min.js',
+      matches: [],
       onload: () => {
         setTimeout(function () {
           Fshare.attachActions();
@@ -18,14 +21,17 @@ const libraryInjector = () => {
     },
     {
       path: 'scripts/vendor/datatables.min.js',
+      matches: [/https:\/\/.*fshare.vn\/folder\//],
       onload: () => {},
     },
     {
       path: 'scripts/vendor/dataTables.scroller.min.js',
+      matches: [/https:\/\/.*fshare.vn\/folder\//],
       onload: () => {},
     },
     {
       path: 'scripts/services/fshare.js',
+      matches: [],
       onload: () => {},
     },
   ];
@@ -34,6 +40,9 @@ const libraryInjector = () => {
     let scriptTag = document.createElement('script');
     scriptTag.src = chrome.runtime.getURL(lib.path);
     scriptTag.onload = lib.onload;
+
+    const currentDomainMatched = lib.matches.filter((regex) => regex.test(location.href));
+    if (lib.matches.length > 0 && currentDomainMatched.length == 0) return;
 
     // Avoid error undefined appendChild
     document;
