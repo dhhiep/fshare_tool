5c43b2d767727187bfd82fef5966a8e5a2f0ee38
Hiep Dinh <hoanghiepitvnn@gmail.com>
2022-04-23 23:57:45 +0700

    Enhance Activity tracking

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -23,18 +23,41 @@ class Activity < ApplicationRecord
       return if Activity::ACTIONS.exclude?(type)
 
       request_data = parse_request_data(response)
-      direct_url = response.body[:location]
       activity_data = {
         action: type,
         url: request_data[:url],
-        file_name: direct_url.to_s.split('/').last,
-        file_ext: direct_url.to_s.split('.').last.to_s.downcase,
+        file_name: extract_name(type, response),
+        file_ext: extract_file_ext(type, response),
       }
 
       create(activity_data)
     end
 
     private
+
+    def extract_name(type, response)
+      case type
+      when :list
+        response.body&.first&.dig('path')&.split('/')&.last
+      when :list_v3
+        response.body&.dig(:current, :name)
+      when :play, :shared_link, :direct_link
+        response.body&.dig(:location).to_s.split('/').last
+      else
+        'Unknown'
+      end
+    end
+
+    def extract_file_ext(type, response)
+      case type
+      when :list, :list_v3
+        ''
+      when :play, :shared_link, :direct_link
+        response.body&.dig(:location).to_s.split('.').last.to_s.downcase
+      else
+        'Unknown'
+      end
+    end
 
     def parse_request_data(response)
       data = response.request_params[:body].presence || response.request_params[:query].presence || {}
